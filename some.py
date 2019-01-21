d1c0edf59010f3a2b3ce32d38b685006637f27a3
TovarnovM <misha198921rip@gmail.com>
2019-01-21 17:22:11 +0300

    Try save pass)

--- some.py
+++ some.py
@@ -2,6 +2,8 @@ class Class4Tst(object):
     def __init__(self, *args, **kwargs):
         self.n = args[0]
 
+#some changes 4 tst git token
+
 if __name__ == "__main__":
     c = Class4Tst(10)
     print(c.n)
