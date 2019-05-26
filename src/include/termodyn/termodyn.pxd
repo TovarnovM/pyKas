e08fe0d49fcdb74c2620a1641aebf6d552e03e1e
TovarnovM <misha198921rip@gmail.com>
2019-05-26 20:33:02 +0300

    termodyn done

--- src/include/termodyn/termodyn.pxd
+++ src/include/termodyn/termodyn.pxd
@@ -2,29 +2,19 @@
 # cython: language_level=3, boundscheck=False, nonecheck=False, cdivision=True, initializedcheck=False
 from tube cimport InterpXY
 
-cdef class DirectBall(object):
-    cdef public dict opts  
-    cdef double I_k,alpha_k,ro,omega,S,d,fi,q,F_0,delta,f,k,R_g,v_T,sigma_T,p_f,p0,l_max,t_max,dt
-    cdef InterpXY dpsi_dz
-
-    cpdef double[:] euler_step(self, double dt, double[:] y, double[:] dy) 
-    cpdef void init_consts(self)
-    cdef void get_dydt(self, double t, double[:] y, double[:] res)
-    cdef int p_f_foo(self, double p, double V)
-    cdef double get_dpsi(self, double z, double dzdt)
-
-
 cdef class DirectBallMany(object):
     cdef public dict opts
     cdef public list dpsi_dz_s
-    cdef double[:] I_ks,alpha_ks,ros,omegas, fs, T_1s, ks
+    cdef public double[:] I_ks,alpha_ks,ros,omegas, fs, T_1s, ks
     cdef public object stop_foo
     
-    cdef double S,d,fi,q,F_0,k,R_g,v_T,sigma_T,p_f,p0,l_max,t_max,dt, omega, W_kam
-    cdef int n_powders
+    cdef public double k_vospl,T_vospl,f_vospl,omega_vospl,w0_sv,S,d,fi,q,F_0,k,R_g,T_stenki,T_cur,sigma_T,p_f,p0,l_max,t_max,dt, omega, W_kam
+    cdef public int n_powders
 
     cpdef double[:] euler_step(self, double dt, double[:] y, double[:] dy)
     cpdef void init_consts(self)
     cdef void get_dydt(self, double t, double[:] y, double[:] res)
     cdef int p_f_foo(self, double p, double V)
     cdef double get_dpsi(self, double z, double dzdt)
+    cpdef (double, double, double, double) set_k_and_stuff(self, double[:] y)
+    cpdef bint stop_condition(self, double t, double[:] y)
--- src/include/termodyn/termodyn.pyx
+++ src/include/termodyn/termodyn.pyx
@@ -8,191 +8,6 @@ from libc.math cimport pi
 from tube cimport InterpXY
 
 
-def get_opts_sample():
-    """
-    Функция для получения примера опций для DirectBall
-    """
-    return {
-        'powder': {
-            'I_k': 0.32,      # импульс конца горения МПа*с
-            'alpha_k': 1.008, # коволюм
-            'ro': 1.6,        # плотность пороха г/см^3   
-            'f': 1.027,       # сила пороха, МДж/кг
-            'etta': 0.228,    # k -1
-            'T_1': 3006.0     # темп. горения ?
-        },
-        'init_cond': {
-            'omega': 23,      # масса навески пороха, г
-            'd':23,           # калибр, мм      
-            'K_zar': 1.08,    # коэфф, учитывающий доп работы
-            'q': 190,         # масса снаряда, г
-            'delta': 800,     # плотность заряжания, кг/м^3
-            'sigma_T': 376,   # Постоянная коэффициента теплоотдачи, Дж*м/кг*К*с
-            'v_T': 0.7,       # Относительная разность температур
-            'p0': 5e6,        # Давление вспышки, Па
-            'p_f': 60e6       # Давление форсирования, Па
-        },
-        'integr_cond': {
-            'l_max': 0.887,   # длина ствола
-            't_max': 0.5,     # на всякий пожарынй
-            'dt': 6e-6        # шаг по времени
-        }
-    }
-
-
-cdef class DirectBall(object):
-    """
-    Решение прямой задачи внутренней баллистики с 1 порохом
-    """
-    
-    def __init__(self, opts, dpsidz, z):
-        self.opts = deepcopy(opts)
-        self.dpsi_dz = InterpXY(z, dpsidz)
-        
-    def get_y0(self):
-        return np.array([self.p0, 0, self.omega/self.delta-self.omega/self.ro, 0, 0, 0])
-      
-    @cython.boundscheck(False)
-    @cython.wraparound(False)  
-    def run(self):
-        """
-        return np.array((много, 6))
-        res[i] = [t, p, l, W, V, psi, z]
-        
-        чтобы сделать слайс для:
-            t res[:,0]
-            p res[:,1]
-            l res[:,2]
-            W res[:,3]
-            V res[:,4]
-            psi res[:,5]
-            z res[:,6]
-        """
-        self.init_consts()
-        cdef double[:] y = self.get_y0()
-        cdef list ys = []
-        cdef list ts = []
-        cdef double[:] k1,k2,k3,k4,yn1
-        k1 = np.empty_like(y)
-        k2 = np.empty_like(y)
-        k3 = np.empty_like(y)
-        k4 = np.empty_like(y)       
-        cdef double t = 0.0, dt = self.dt        
-        cdef int i,j
-        while True:
-            ys.append(y)
-            ts.append(t)
-            if t >= self.t_max or y[3] >= self.l_max:
-                break
-            self.get_dydt(t, y, k1)
-            self.get_dydt(t+0.5*dt, self.euler_step(0.5*dt, y, k1), k2)
-            self.get_dydt(t+0.5*dt, self.euler_step(0.5*dt, y, k2), k3)
-            self.get_dydt(t+dt, self.euler_step(dt, y, k3), k4)
-            yn1 = np.empty_like(y)
-            for i in range(len(yn1)):
-                yn1[i] = y[i] + (k1[i]+2*k2[i]+2*k3[i]+k4[i])*dt/6
-            t += dt
-            y = yn1
-        res = np.zeros((len(ts), len(y)+1), dtype=np.double)
-        for i in range(len(ts)):
-            y = ys[i]
-            t = ts[i]
-            res[i,0] = t 
-            for j in range(len(y)):
-                res[i,j+1] = y[j]
-        return res
-
-    @cython.boundscheck(False)
-    @cython.wraparound(False)  
-    cpdef double[:] euler_step(self, double dt, double[:] y, double[:] dy):
-        cdef double[:] res = np.empty_like(y)
-        cdef int i
-        for i in range(len(y)):
-            res[i] = y[i] + dt*dy[i]
-        return res
-   
-    cpdef void init_consts(self):
-        powder = self.opts['powder']
-        self.I_k = powder['I_k'] * 10**6
-        self.alpha_k = powder['alpha_k']/10**3
-        self.ro = powder['ro']*10**3
-        self.f = powder['f']*10**6
-        self.k = powder['etta']+1
-        self.R_g = self.f / powder['T_1']
-        
-        init_cond = self.opts['init_cond']
-        self.omega = init_cond['omega'] / 10**3
-        self.d = init_cond['d'] / 10**3
-        self.S = pi*self.d**2 / 4
-        self.q = init_cond['q']/10**3
-        self.fi = init_cond['K_zar'] + 1/3 * self.omega / self.q
-        self.delta = init_cond['delta']
-        self.F_0 = 4 * self.omega / self.delta / self.d
-        self.v_T = init_cond['v_T']
-        self.sigma_T = init_cond['sigma_T']        
-        self.p0 = init_cond['p0']
-        self.p_f = init_cond['p_f']
-        
-        integr_cond = self.opts['integr_cond']
-        self.l_max = integr_cond['l_max']
-        self.t_max = integr_cond['t_max']
-        self.dt = integr_cond['dt']
-    
-    @cython.boundscheck(False)
-    @cython.wraparound(False)    
-    cdef void get_dydt(self, double t, double[:] y, double[:] res):
-        cdef double p = y[0]
-
-        cdef double l = y[1]
-        cdef double W = y[2]
-        cdef double V = y[3]
-        
-        cdef double psi = y[4]
-        cdef double z = y[5]
-        
-        cdef double dz = p/self.I_k
-        
-        if psi > 1:
-            psi = 1
-            dz = 0
-        elif psi < 0:
-            psi = 0
-                
-        cdef double dpsi = self.get_dpsi(z, dz) if 0 <= psi <= 1 else 0
-        cdef double dW = (1-self.alpha_k * self.ro)/self.ro*self.omega*dpsi + self.S*V
-        cdef double dV = self.S*p/self.fi/self.q * self.p_f_foo(p, V)
-        cdef double F = self.F_0 + pi*l*self.d
-        cdef double dp = (self.f*self.omega*dpsi \
-                        - (self.k-1)*self.v_T*self.sigma_T*F*p/self.R_g \
-                        - self.k*p*dW)/W
-        res[0] = dp
-        res[1] = V
-        res[2] = dW
-        res[3] = dV
-        res[4] = dpsi
-        res[5] = dz
-        
-    def get_dydt_tst(self, t, y):
-        """
-        d[p,l,W,V,psi,z]/dt
-        """
-        cdef double[:] res = np.empty_like(y)
-        self.get_dydt(t, y, res)
-        return np.asarray(res)
-    
-    cdef int p_f_foo(self, double p, double V):
-        if V > 1e-6:
-            return 1
-        if p < self.p_f:
-            return 0
-        return 1
-    
-    cdef double get_dpsi(self, double z, double dzdt):
-        return self.dpsi_dz.get_v(z) * dzdt
-#         cdef double k1=0.811, k2=0.505, l1=0.081, l2=-1.024
-#         cdef double res = k1*(1+2*l1*z) if z<=1 else k2*(1+2*l2*(z-1)) if z<1.488 else 0
-#         return res*dz
-            
 def get_dpsi_array(k1=0.811, k2=0.505, l1=0.081, l2=-1.024, z_k=1.488, n=1000):
     """
     k1=0.811, k2=0.505, l1=0.081, l2=-1.024, z_k=1.488, n=1000
@@ -201,7 +16,7 @@ def get_dpsi_array(k1=0.811, k2=0.505, l1=0.081, l2=-1.024, z_k=1.488, n=1000):
     zs = np.linspace(0, z_k, n)
     dpsidz = np.empty_like(zs)
     for i, z in enumerate(zs):
-        dpsidz[i] = k1*(1+2*l1*z) if z<=1 else k2*(1+2*l2*(z-1)) if z<1.488 else 0
+        dpsidz[i] = k1*(1+2*l1*z) if z<=1 else k2*(1+2*l2*(z-1)) if z<z_k else 0
     return dpsidz, zs
 
 
@@ -241,9 +56,13 @@ def get_optsmany_sample():
             'q': 190,         # масса снаряда, г
             'W_kam': 28.75,   # объем каморы, см^3
             'sigma_T': 376,   # Постоянная коэффициента теплоотдачи, Дж*м/кг*К*с
-            'v_T': 0.7,       # Относительная разность температур
+            'T_stenki': 293,  # Температура стенки, К
+            'p_f': 60e6,      # Давление форсирования, Па
+
             'p0': 5e6,        # Давление вспышки, Па
-            'p_f': 60e6       # Давление форсирования, Па
+            'k_vospl': 1.22,  # Коэффициент адиабаты газов воспламенителя
+            'f_vospl': 0.26,  # Cила воспламенителя, МДж/кг
+            'T_vospl': 2427   # Температура газов воспламенителя, К
         },
         'integr_cond': {
             'l_max': 0.887,   # длина ствола
@@ -285,9 +104,13 @@ def get_optsmany_sample():
             'q': 190,         # масса снаряда, г
             'W_kam': 28.75,   # объем каморы, см^3
             'sigma_T': 376,   # Постоянная коэффициента теплоотдачи, Дж*м/кг*К*с
-            'v_T': 0.7,       # Относительная разность температур
+            'T_stenki': 293,  # Температура стенки, К
+            'p_f': 60e6,      # Давление форсирования, Па
+
             'p0': 5e6,        # Давление вспышки, Па
-            'p_f': 60e6       # Давление форсирования, Па
+            'k_vospl': 1.22,  # Коэффициент адиабаты газов воспламенителя
+            'f_vospl': 0.26,  # Cила воспламенителя, МДж/кг
+            'T_vospl': 2427   # Температура газов воспламенителя, К
         },
         'integr_cond': {
             'l_max': 0.887,   # длина ствола
@@ -337,9 +160,13 @@ cdef class DirectBallMany(object):
                                 'q': 190,         # масса снаряда, г
                                 'W_kam': 28.75,   # объем каморы, см^3
                                 'sigma_T': 376,   # Постоянная коэффициента теплоотдачи, Дж*м/кг*К*с
-                                'v_T': 0.7,       # Относительная разность температур
+                                'T_stenki': 293,  # Температура стенки, К
+                                'p_f': 60e6,      # Давление форсирования, Па
+
                                 'p0': 5e6,        # Давление вспышки, Па
-                                'p_f': 60e6       # Давление форсирования, Па
+                                'k_vospl': 1.22,  # Коэффициент адиабаты газов воспламенителя
+                                'f_vospl': 0.26,  # Cила воспламенителя, МДж/кг
+                                'T_vospl': 2427   # Температура газов воспламенителя, К
                             },
                             'integr_cond': {
                                 'l_max': 0.887,   # длина ствола
@@ -359,12 +186,16 @@ cdef class DirectBallMany(object):
         cdef int i
         for i in range(self.n_powders):
             w0_sv -= self.omegas[i]/self.ros[i]
+        if w0_sv <= 0:
+            raise AttributeError(f'Неправильные данные, начальный свободный объем меньше 0')
         res = np.zeros(4+2*self.n_powders)
+        self.w0_sv = w0_sv
+        self.omega_vospl = self.p0*w0_sv/self.f_vospl
         res[0] = self.p0
         res[2] = w0_sv
         return res
 
-    def stop_condition(self, t,  y):
+    cpdef bint stop_condition(self, double t, double[:] y):
         if self.stop_foo is not None:
             return self.stop_foo(t, y) 
         return t >= self.t_max or y[1] >= self.l_max
@@ -384,9 +215,11 @@ cdef class DirectBallMany(object):
             V res[:,4]
             psi1 res[:,5]
             z1 res[:,6]
+            psi2 res[:,7]
+            z2 res[:,8]
             ...
-            psiN res[:,5+N]
-            zN res[:,6+N]
+            psiN res[:,5+2*N]
+            zN res[:,6+2*N]
         """
         self.init_consts()
         cdef double[:] y = self.get_y0()
@@ -406,12 +239,13 @@ cdef class DirectBallMany(object):
             ts.append(t)
             if self.stop_condition(t, y):
                 break
+            self.set_k_and_stuff(y)
             self.get_dydt(t, y, k1)
             self.get_dydt(t+0.5*dt, self.euler_step(0.5*dt, y, k1), k2)
             self.get_dydt(t+0.5*dt, self.euler_step(0.5*dt, y, k2), k3)
             self.get_dydt(t+dt, self.euler_step(dt, y, k3), k4)
             yn1 = np.empty_like(y)
-            for i in range(len(yn1)):
+            for i in range(yn1.shape[0]):
                 yn1[i] = y[i] + (k1[i]+2*k2[i]+2*k3[i]+k4[i])*dt/6
             t += dt
             y = yn1
@@ -429,7 +263,7 @@ cdef class DirectBallMany(object):
     cpdef double[:] euler_step(self, double dt, double[:] y, double[:] dy):
         cdef double[:] res = np.empty_like(y)
         cdef int i
-        for i in range(len(y)):
+        for i in range(y.shape[0]):
             res[i] = y[i] + dt*dy[i]
         return res
    
@@ -474,21 +308,55 @@ cdef class DirectBallMany(object):
         self.W_kam = init_cond['W_kam']/100**3
         self.fi = init_cond['K_zar'] + 1/3 * self.omega / self.q
         self.F_0 = 4 * self.W_kam / self.d
-        self.v_T = init_cond['v_T']
+        self.T_stenki = init_cond['T_stenki']
         self.sigma_T = init_cond['sigma_T']        
         self.p0 = init_cond['p0']
         self.p_f = init_cond['p_f']
+
+        self.k_vospl = init_cond['k_vospl']
+        self.f_vospl = init_cond['f_vospl']*10**6
+        self.T_vospl = init_cond['T_vospl']
         
         integr_cond = self.opts['integr_cond']
         self.l_max = integr_cond['l_max']
         self.t_max = integr_cond['t_max']
         self.dt = integr_cond['dt']
+
+    cpdef (double, double, double, double) set_k_and_stuff(self, double[:] y):
+        """Функция обновляет показатели адиабаты, и газвоую постоянную, согласно массам сговревших порохов
+        и массе воспламенителя
+
+        y[] # [p,  l, W, V, psi0, z0, psi1, z1, ..., psiN, zN]
+            # [0,  1, 2, 3,    4,  5,    6,  7, ...,  4+2*N, 5+2*N]
+
+        return (k, T, R_g, f_sred)
+        """
+        cdef double omega_sum = self.omega_vospl
+        cdef double psi_i, ves
+        for i in range(self.n_powders):
+            psi_i = y[4+2*i]
+            omega_sum += psi_i * self.omegas[i]
+
+        self.k = self.k_vospl * self.omega_vospl / omega_sum
+        cdef double T_1 = self.T_vospl * self.omega_vospl / omega_sum
+        cdef double f_sred = self.f_vospl * self.omega_vospl / omega_sum
+        cdef double W = y[2]
+        for i in range(self.n_powders):   
+            psi_i = y[4+2*i] 
+            ves = psi_i * self.omegas[i] / omega_sum
+            self.k += ves * self.ks[i]
+            T_1 += ves * self.T_1s[i]
+            f_sred += ves * self.fs[i]
+            W -= self.alpha_ks[i] * psi_i * self.omegas[i]
+        self.R_g = f_sred / T_1
+        self.T_cur = y[0]*W/(omega_sum*self.R_g)
+        return (self.k, self.T_cur, self.R_g, f_sred)
     
     @cython.boundscheck(False)
     @cython.wraparound(False)    
     cdef void get_dydt(self, double t, double[:] y, double[:] res):
         # [p,  l, W, V, psi0, z0, psi1, z1, ..., psiN, zN]
-        # [0,  1, 2, 3,    4,  5,    6,  7, ...,  4+N, 5+N]
+        # [0,  1, 2, 3,    4,  5,    6,  7, ...,  4+2*N, 5+2*N]
         cdef double p = y[0]
         cdef double psi_i, z_i
         cdef int i
@@ -514,7 +382,7 @@ cdef class DirectBallMany(object):
         cdef double dW = self.S*V
         cdef double dV = self.S*p/self.fi/self.q * self.p_f_foo(p, V)
         cdef double F = self.F_0 + pi*l*self.d
-        cdef double dp = -(self.k-1)*self.v_T*self.sigma_T*F*p/self.R_g
+        cdef double dp = -(self.k-1)*(1-self.T_stenki/self.T_cur)*self.sigma_T*F*p/self.R_g
         cdef double dpsi
         for i in range(self.n_powders):
             dpsi = res[4+2*i]
